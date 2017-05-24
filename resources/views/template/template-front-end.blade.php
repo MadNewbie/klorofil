e894a0dba7cb8e9124bdff3244a6fd0ce5a3bdc2
rachmad <rachmad@TCKanopi-Office2>
2017-05-24 21:35:50 +0700

    test commit dari PC lagi

--- resources/views/template/template-front-end.blade.php
+++ resources/views/template/template-front-end.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>@yield ('title')</title>
-        <link rel="stylesheet" href="{{URL::to('general/bootstrap/dist/css/bootstrap.min.css')}}"/>
+        <link rel="stylesheet" href="{{URL::to('general/bootstrap/css/bootstrap.min.css')}}"/>
         <link rel="stylesheet" href="{{URL::to('front-end/mobirise/css/mbr-additional.css')}}"/>
         <link rel="stylesheet" href="{{URL::to('front-end/tether/tether.min.css')}}"/>
         <link rel="stylesheet" href="{{URL::to('front-end/style.css')}}"/>
@@ -16,7 +16,7 @@
         </div>
         @yield('script')
         <script type='text/javascript' src="{{URL::to('general/smooth-scroll/smooth-scroll.js')}}"></script>
-        <script type='text/javascript' src="{{URL::to('general/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+        <script type='text/javascript' src="{{URL::to('general/bootstrap/js/bootstrap.min.js')}}"></script>
         <script type='text/javascript' src="{{URL::to('general/jquery/dist/jquery.min.js')}}"></script>
     </body>
 </html>
