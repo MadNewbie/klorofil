de09f0863fd2d466d27e95743bcbf74b7ef0f5b2
Kristian Amadeus <amadeus.kristian@gmail.com>
2017-12-02 11:54:04 +0700

    User-related code addition.

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\User;
+//use App\Models\User;
+use App\Role;
+//use App\Models\User;
+use App\Permission;
+//use App\Models\Permission;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
@@ -10,15 +15,29 @@ use Validator;
 
 class UserManagementController extends Controller
 {
+    //User management CRUD section
     public function getUsermanIndex() {
-        return view('akun.usersmanagement');
+        $sectionTitle="Users Management";
+        $users=User::orderBy('id')->paginate(10);
+        return view('akun.usersmanagement',[
+
+        ]);
     }
 
+    //User Role management CRUD section
     public function getRolesmanIndex(){
-        return view('akun.rolesmanagement');
+        $sectionTitle="Role(s) Management";
+        $roles=Role::orderBy('id')->paginate(10);
+        return view('akun.rolesmanagement',[
+          'jenis_users'=>$roles,
+        ]);
     }
 
+    //User Permission management CRUD section
     public function getPermissionsmanIndex(){
-        return view('akun.rolesmanagement');
+        $sectionTitle="Permission(s) Management";
+        return view('akun.permissionsmanagement',[
+
+        ]);
     }
 }
